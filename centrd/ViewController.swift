6384690d1a2a8d212d542a9a1bfea75844f18fb9
Evan Pun <evan91@gmail.com>
2016-02-14 01:12:24 -0800

    Initial Commit

---
+++ centrd/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  centrd
+//
+//  Created by Evan Pun on 2/14/16.
+//  Copyright (c) 2016 Evan Pun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
